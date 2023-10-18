42a799cb57e49b705437d171c9dab98972056538
Choi Seongwon <vlflalektzja@naver.com>
2023-10-18 10:51:35 +0900

    feat: 프로젝트 초기 설정

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Team-BGP-DIDI-BE
 박격포 팀 백엔드
+
+## 프로젝트 환경
+- Spring Boot 3.1.4
+- Java 17
---
+++ src/main/java/com/bgp/didibe/DidiBeApplication.java
@@ -0,0 +1,13 @@
+package com.bgp.didibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidiBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DidiBeApplication.class, args);
+    }
+
+}
